f043432507191d734d944b227d85bb303d756b01
Ahmed Agabani <agabani@live.com>
2016-03-13 13:25:56 +0000

    Basket now uses builders

--- Api/Modules/BasketModule.cs
+++ Api/Modules/BasketModule.cs
@@ -24,76 +24,65 @@ namespace Api.Modules
 
         public Entity Handle(BasketAddModel model)
         {
-            Account account;
-            var isExistingAccount = TryGetAccount(out account);
+            var account = GetAccount();
 
             var basket = BasketRepository.Get(account);
             basket.AddItem(ItemRepository.Get(model.Id));
             BasketRepository.Save(account, basket);
 
-            return new EntityBuilder()
-                .WithClass("basket")
-                .WithClass("collection")
-                .WithProperty("price", Checkout.GetTotal(basket).Units)
-                .WithEntity(BuildEntities(basket, account, isExistingAccount).Select<Entity, Func<Entity>>(e => () => e))
-                .WithLink(() => LinkFactory.Create("basket", true))
-                .WithLink(() => LinkFactory.Create("items", false))
-                .Build();
+            return BuildEntity(basket, account);
         }
 
         public Entity Handle()
         {
-            Account account;
-            TryGetAccount(out account);
+            var account = GetAccount();
 
             var basket = BasketRepository.Get(account);
 
+            return BuildEntity(basket, account);
+        }
+
+        private Entity BuildEntity(Basket basket, Account account)
+        {
             return new EntityBuilder()
                 .WithClass("basket")
                 .WithClass("collection")
                 .WithProperty("price", Checkout.GetTotal(basket).Units)
-                .WithEntity(BuildEntities(basket).Select<Entity, Func<Entity>>(e => () => e))
+                .WithEntity(BuildEntities(basket, account).Select<Entity, Func<Entity>>(e => () => e))
                 .WithLink(() => LinkFactory.Create("basket", true))
                 .WithLink(() => LinkFactory.Create("items", false))
                 .Build();
         }
 
-        private bool TryGetAccount(out Account account)
+        private Account GetAccount()
         {
             StringValues stringValues;
 
-            account = Request.Headers.TryGetValue("authorization", out stringValues)
+            return Request.Headers.TryGetValue("authorization", out stringValues)
                 ? AccountRepository.GetByToken(Guid.Parse(stringValues.First().Split(' ').Last()))
                 : AccountRepository.CreateAnonymous();
-
-            return stringValues != default(StringValues);
         }
 
-        private IEnumerable<Entity> BuildEntities(Basket basket, Account account, bool isAuthenticated)
+        private IEnumerable<Entity> BuildEntities(Basket basket, Account account)
         {
-            var entities = basket.Items
-                .Select(item => item.Id)
-                .Select(itemId => new AnemicItemModule(Request, itemId).Handle())
-                .ToArray();
+            var builder = new EntityBuilder();
 
-            var httpEntity = new Entity
+            if (!Request.Headers.ContainsKey("authorization"))
             {
-                Class = new[] {"http"},
-                Properties = new Dictionary<string, dynamic>
-                {
-                    {"authorization", $"Basic {account.Token}"}
-                }
-            };
+                builder.WithEntity(() =>
+                    new EntityBuilder()
+                        .WithClass("http")
+                        .WithProperty("authorization", $"Basic {account.Token}")
+                        .Build());
+            }
 
-            return isAuthenticated ? entities : new[] {httpEntity}.Concat(entities).ToArray();
-        }
+            builder
+                .WithEntity(basket.Items
+                    .Select(item => item.Id)
+                    .Select(id => new AnemicItemModule(Request, id).Handle())
+                    .Select<Entity, Func<Entity>>(entity => () => entity));
 
-        private IEnumerable<Entity> BuildEntities(Basket basket)
-        {
-            return basket.Items
-                .Select(item => item.Id)
-                .Select(s => new AnemicItemModule(Request, s).Handle())
-                .ToArray();
+            return builder.Build().Entities;
         }
     }
 }
